7d10e4a7f2f3e79ce91e098dd0c0061d5271d386
赤嶺 一馬 <kazuma@akanmi.net>
2024-02-21 21:56:31 +0900

    feat(entry form): convert template

---
+++ app/resources/views/template.blade.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+
+    <meta charset="utf-8">
+
+    <meta name="robots" content="INDEX, FOLLOW, NOYDIR, NOODP">
+    <meta name="description" content="マルイチ株式会社は建設用仮設資材・住宅用ビケ足場など建設現場その他で使用される各種仮設資材のレンタル、リース事業を行っています。" />
+    <meta name="keywords" content="建設関係,姫路,神戸,採用,マルイチ" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>新卒採用エントリーフォーム | 仮設足場・仮設資材のレンタル｜マルイチ株式会社。｜採用特設サイト</title>
+    <link rel="apple-touch-icon" href="https://www.maruichi-yg.com/common/common/img/icon.png">
+    <link rel="stylesheet" type="text/css" href="https://www.maruichi-yg.com/common/css/reset.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://www.maruichi-yg.com/common/css/common_rec.css?date=20231120131738">
+    <link rel="stylesheet" type="text/css"
+        href="https://www.maruichi-yg.com/common/css/common_smp_rec.css?date=20231120132508"
+        media="only screen and (max-width: 480px)">
+    <link rel="stylesheet" type="text/css" href="https://www.maruichi-yg.com/common/css/libs/animate.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Cinzel|Open+Sans|Open+Sans+Condensed:700|Poppins|Reem+Kufi"
+        rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://www.maruichi-yg.com/common/font/brandongrotesque.css">
+    <link rel="stylesheet" type="text/css" href="https://www.maruichi-yg.com/common/font/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="https://www.maruichi-yg.com/common/font/notosans.css">
+
+    <link rel="stylesheet" type="text/css" href="https://www.maruichi-yg.com/common/plugin/slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="https://www.maruichi-yg.com/common/plugin/slick/slick-theme.css">
+    <link rel="stylesheet" type="text/css" href="https://www.maruichi-yg.com/common/plugin/colorbox/colorbox.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script src="https://www.maruichi-yg.com/common/js/libs/jquery.simple-sidebar.min.js"></script>
+    <script src="https://www.maruichi-yg.com/common/js/libs/imagesloaded.pkgd.min.js"></script>
+    <script src="https://www.maruichi-yg.com/common/js/libs/velocity.min.js"></script>
+    <script src="https://www.maruichi-yg.com/common/js/libs/jquery.cookie.js"></script>
+    <script src="https://www.maruichi-yg.com/common/plugin/slick/slick.min.js"></script>
+    <script src="https://www.maruichi-yg.com/common/plugin/colorbox/jquery.colorbox-min.js"></script>
+
+    <script type="text/javascript" src="https://ajaxzip3.github.io/ajaxzip3.js" charset="UTF-8"></script>
+    <script type="text/javascript" src="each.js"></script>
+    @yield('head')
+</head>
+
+<body id="form">
+
+    <nav id="gnavi" class="smp-only">
+
+        <div id="gnavi-contents" class="content-width pt-30">
+
+            <div class="gnavi-header">
+                <div id="navbar-close-button"><span class="cross"></span></div>
+            </div>
+
+            <ul class="gnavi-list">
+                <li><a href="https://www.maruichi-yg.com/recruit/">ホーム</a></li>
+                <li><a href="https://www.maruichi-yg.com/recruit/jobcategory/">仕事を知る</a></li>
+                <li><a href="https://www.maruichi-yg.com/recruit/interview/">人を知る</a></li>
+                <li><a href="https://www.maruichi-yg.com/recruit/message/">代表からのメッセージ</a></li>
+                <li><a href="https://www.maruichi-yg.com/recruit/dream/">夢を叶える我社のチカラ</a></li>
+                <li><a href="https://www.maruichi-yg.com/recruit/welfare/">福利厚生</a></li>
+                <li><a href="http://www.maruichi-yg.com/company/" target="_blank"><i class="fa fa-clone"
+                            aria-hidden="true"></i> 企業情報</a></li>
+                <li><a href="https://www.maruichi-yg.com/recruit/entry/">エントリー</a></li>
+                <li><a href="https://www.maruichi-yg.com/recruit/contact/">お問い合わせ</a></li>
+                <li><a href="https://www.maruichi-yg.com/recruit/privacy-policy/">個人情報取扱いに関する表記</a></li>
+            </ul>
+
+        </div>
+
+    </nav>
+
+    <div id="container" class="subpage">
+
+        <div id="header">
+
+            <div id="header-contents" class="">
+
+                <div id="navbar-smp" class="smp-only"><i class="fa fa-fw fa-bars" aria-hidden="true"></i></div>
+
+                <h1 class="header-logo">
+                    <a href="https://www.maruichi-yg.com/recruit/"><img title="img title"
+                            src="https://www.maruichi-yg.com/common/img/logo_rec.png" width="228" height="48"></a>
+                </h1>
+
+                <nav id="header-gnavi">
+                    <ul class="gnavi-list">
+                        <li><a href="https://www.maruichi-yg.com/recruit/jobcategory/">仕事を知る</a></li>
+                        <li><a href="https://www.maruichi-yg.com/recruit/interview/">人を知る</a></li>
+                        <li><a href="https://www.maruichi-yg.com/recruit/message/">代表からのメッセージ</a></li>
+                        <li><a href="https://www.maruichi-yg.com/recruit/dream/">夢を叶える我社のチカラ</a></li>
+                        <li><a href="https://www.maruichi-yg.com/recruit/welfare/">福利厚生</a></li>
+                        <li><a href="http://www.maruichi-yg.com/company/" target="_blank"><i class="fa fa-clone"
+                                    aria-hidden="true"></i> 企業情報</a></li>
+                    </ul>
+
+                    <div class="entry-button panel-button"><a href="https://www.maruichi-yg.com/recruit/entry/"><i
+                                class="fa fa-pencil" aria-hidden="true"></i><span>ENTRY</span></a></div>
+                    <div class="contact-button panel-button"><a href="https://www.maruichi-yg.com/recruit/contact/"><i
+                                class="fa fa-envelope-o" aria-hidden="true"></i><span>CONTACT</span></a></div>
+                </nav>
+
+            </div>
+
+        </div>
+        <div id="sections">
+
+            <div id="main" class="main-container">
+
+                <div class="title-area">
+                    @yield('title-area')
+                </div>
+
+                <div class="main-area form-container">
+                    @yield('main-area')
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <a id="totop" class="to-top" href="#main"><i class="fa fa-angle-up" aria-hidden="true"></i></a>
+
+        <div id="footer">
+
+            <div class="footer-contents">
+
+                <img src="https://www.maruichi-yg.com/common/img/footer_logo.png" class="footer-logo">
+
+                <h3 class="footer-title">“チャレンジする人が未来を創る”</h3>
+
+                <a href="https://www.maruichi-yg.com/recruit/entry/" class="footer-entry-button"><i
+                        class="fa fa-pencil-square-o" aria-hidden="true"></i><span>エントリー</span></a>
+
+                <ul class="footer-nav">
+                    <li><a href="https://www.maruichi-yg.com/recruit/privacy-policy/">プライバシーポリシー</a></li>
+                    <li><a href="http://www.maruichi-yg.com/" target="_blank">コーポレートサイト <i class="fa fa-clone"
+                                aria-hidden="true"></i></a></li>
+                </ul>
+
+            </div>
+
+        </div>
+
+        <div id="copyright" class="upper">copyright &copy; MARUICHI co., ltd. All rights reserved.</div>
+        <!-- /container -->
+    </div>
+
+    <script type="text/javascript">
+        var selected = '',
+            userAgent = 'pc',
+            dirname = 'newgraduate',
+            parent = 'entry';
+
+        if (parent.length <= 0) {
+            selected = dirname;
+        } else {
+            selected = parent;
+        }
+
+        (function($) {
+
+            var windowHeight = window.innerHeight,
+                windowWidth = window.innerWidth;
+
+            var totopFlag = false;
+
+
+            $(function() {
+
+
+                $('#totop').attr('href', '#container');
+
+                $(window).scroll(function() {
+                    if ($(window).scrollTop() > windowHeight && !totopFlag) {
+                        totopFlag = true;
+                        $('#totop').stop().fadeIn('fast');
+                    } else if ($(window).scrollTop() < windowHeight && totopFlag) {
+                        totopFlag = false;
+                        $('#totop').stop().fadeOut('fast');
+                    }
+                });
+
+                $('#banner-slider').slick({
+                    arrows: true,
+                    autoplay: true,
+                    autoplaySpeed: 5000,
+                    dots: true,
+                    infinite: true,
+                    slidesToShow: 5,
+                    slidesToScroll: 1
+                });
+
+                if (userAgent !== 'phone') {
+
+                    // $('.footer-tel').attr('href', '#footer');
+
+                    // $(window).scroll(function () {
+                    //     if( $(window).scrollTop() > gnaviPoint && !gnaviFlag ) {
+                    //         gnaviFlag = !gnaviFlag;
+                    //         $('#gnavi').stop().slideDown('fast');
+                    //         $('#totop').stop().fadeIn('slow');
+                    //     } else if ( $(window).scrollTop() < gnaviPoint && gnaviFlag ) {
+                    //         gnaviFlag = !gnaviFlag;
+                    //         $('#gnavi').stop().slideUp('fast');
+                    //         $('#totop').stop().fadeOut('slow');
+                    //     }
+                    // });
+
+                } else {
+
+                    // $('.header-menu').insertBefore('#gnavi .gnavi-list');
+
+                    $.fn.simpleSidebar.settings.animation.duration = 500;
+
+                    $('#gnavi').simpleSidebar({
+                        opener: '#navbar-smp',
+                        wrapper: '#sections',
+                        sidebar: {
+                            align: 'left',
+                            width: 414,
+                            closingLinks: '#navbar-close-button',
+                            css: {
+                                zIndex: 3000
+                            }
+                        }
+                    });
+
+                    $('#header').append(
+                        '<a href="#navigation" id="panel-btn"><span id="panel-btn-icon"></span></a>');
+
+                    $("#panel-btn").click(function() {
+                        $("body").toggleClass('coverd');
+                        $("#gnavi").toggleClass('show');
+                        $("#panel-btn-icon").toggleClass("close");
+                        return false;
+                    });
+                }
+
+                if (selected.length > 0) {
+
+                    $('.gnavi-list').find('.' + selected).addClass('selected');
+
+                }
+
+                $('body').on('click', 'a[href^=#]', function() {
+                    var href = $(this).attr('href'),
+                        target = $(href === '#' || href === '' ? 'html' : href);
+                    target.velocity('scroll', {
+                        duration: 1200,
+                        offset: -76,
+                        easing: 'ease'
+                    });
+                    // target.velocity('scroll', { duration: 1200, easing: 'ease' });
+                    return false;
+                });
+
+            });
+
+        })(jQuery);
+    </script>
+
+    <script>
+        // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        // })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        // ga('create', 'analytics id', 'auto');
+        // ga('send', 'pageview');
+    </script>
+</body>
+
+</html>
