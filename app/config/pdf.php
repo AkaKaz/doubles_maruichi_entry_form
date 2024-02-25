cf8055e2b60ae0c3008871ea34f76e6b9e5aa840
赤嶺 一馬 <kazuma@akanmi.net>
2024-02-26 03:51:48 +0900

    chore: setting pdf page size B5

--- app/config/pdf.php
+++ app/config/pdf.php
@@ -2,13 +2,13 @@
 
 return [
     'mode'                     => '',
-    'format'                   => 'A4',
+    'format'                   => 'B5',
     'default_font_size'        => '12',
     'default_font'             => 'sans-serif',
-    'margin_left'              => 10,
-    'margin_right'             => 10,
-    'margin_top'               => 10,
-    'margin_bottom'            => 10,
+    'margin_left'              => 0,
+    'margin_right'             => 0,
+    'margin_top'               => 0,
+    'margin_bottom'            => 0,
     'margin_header'            => 0,
     'margin_footer'            => 0,
     'orientation'              => 'P',
