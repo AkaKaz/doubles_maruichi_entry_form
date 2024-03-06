c6e6f74bada4311fbde502b27ba79bf248c5d56d
赤嶺 一馬 <kazuma@akanmi.net>
2024-03-06 19:17:27 +0900

    feat(pdf): setting pdf size B5 to A4

--- app/config/pdf.php
+++ app/config/pdf.php
@@ -2,7 +2,7 @@
 
 return [
     'mode'                     => '',
-    'format'                   => 'B5',
+    'format'                   => 'A4',
     'default_font_size'        => '12',
     'default_font'             => 'sans-serif',
     'margin_left'              => 0,
