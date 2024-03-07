401b87b4e3790d6299668513e49282801b9e2bfb
赤嶺 一馬 <kazuma@akanmi.net>
2024-03-07 20:33:03 +0900

    refactor: route definition

--- app/routes/web.php
+++ app/routes/web.php
@@ -17,16 +17,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => redirect()->route('mid-career.index'));
 
-Route::prefix('mid-career')->group(function () {
-    Route::get('/', [MidCareerEntryController::class, 'index'])->name('mid-career.index');
-    Route::post('/back', [MidCareerEntryController::class, 'back'])->name('mid-career.back');
-    Route::post('/confirm', [MidCareerEntryController::class, 'confirm'])->name('mid-career.confirm');
-    Route::post('/complete', [MidCareerEntryController::class, 'complete'])->name('mid-career.complete');
+Route::group([
+    'prefix' => 'mid-career',
+    'as' => 'mid-career.',
+], function () {
+    Route::get('/', [MidCareerEntryController::class, 'index'])->name('index');
+    Route::post('/back', [MidCareerEntryController::class, 'back'])->name('back');
+    Route::post('/confirm', [MidCareerEntryController::class, 'confirm'])->name('confirm');
+    Route::post('/complete', [MidCareerEntryController::class, 'complete'])->name('complete');
 });
 
-Route::prefix('new-graduate')->group(function () {
-    Route::get('/', [NewGraduateEntryController::class, 'index'])->name('new-graduate.index');
-    Route::post('/back', [NewGraduateEntryController::class, 'back'])->name('new-graduate.back');
-    Route::post('/confirm', [NewGraduateEntryController::class, 'confirm'])->name('new-graduate.confirm');
-    Route::post('/complete', [NewGraduateEntryController::class, 'complete'])->name('new-graduate.complete');
+Route::group([
+    'prefix' => 'new-graduate',
+    'as' => 'new-graduate.',
+], function () {
+    Route::get('/', [NewGraduateEntryController::class, 'index'])->name('index');
+    Route::post('/back', [NewGraduateEntryController::class, 'back'])->name('back');
+    Route::post('/confirm', [NewGraduateEntryController::class, 'confirm'])->name('confirm');
+    Route::post('/complete', [NewGraduateEntryController::class, 'complete'])->name('complete');
 });
